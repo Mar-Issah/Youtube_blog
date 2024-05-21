6c1fe0b42a038f4021c88cffa17c4208c101378f
Mar-Issah <masy370@gmail.com>
2024-05-20 22:19:54 -0600

    Create signin and signup pages

--- Home.py
+++ Home.py
@@ -1,5 +1,21 @@
+import streamlit as st
+from dotenv import load_dotenv
+
+# Apply styles
+with open('./styles/styles.css') as file:
+    style = file.read()
+    st.markdown(f"""<style>{style}</style>""", unsafe_allow_html=True)
+
 def main():
     load_dotenv()
+    st.set_page_config(page_title="Youtube Blog", page_icon='🎥🔴')
+
+    st.header("Youtube Blog Generator")
+
+    # with st.form("my_form"):
+    #     st.write("Inside the form")
+
+
 
 if __name__ == '__main__':
     main()
