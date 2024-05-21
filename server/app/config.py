e0afa922d2a6925d9a48cdf924b21ac28a37d426
Mar-Issah <masy370@gmail.com>
2024-05-21 17:09:11 -0600

    Built the signin route

--- server/app/config.py
+++ server/app/config.py
@@ -3,3 +3,5 @@
 class Config:
     SQLALCHEMY_DATABASE_URI = 'mysql://root:siya@localhost/youtube_blog'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY =  os.getenv("SECRET_KEY")
+    JWT_SECRET_KEY =  os.getenv("SECRET_KEY")
