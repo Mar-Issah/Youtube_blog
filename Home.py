7f7665902b70b06ad501a8ba16f1efabb7f18727
Mar-Issah <masy370@gmail.com>
2024-05-20 22:49:41 -0600

    Add Pipfile and Pipfile,loc to .gitgnore file

--- Home.py
+++ Home.py
@@ -1,20 +1,21 @@
 import streamlit as st
 from dotenv import load_dotenv
 
-# Apply styles
-with open('./styles/styles.css') as file:
-    style = file.read()
-    st.markdown(f"""<style>{style}</style>""", unsafe_allow_html=True)
-
 def main():
     load_dotenv()
-    st.set_page_config(page_title="Youtube Blog", page_icon='🎥🔴')
+    st.set_page_config(page_title="Youtube Blog", page_icon='🎥🔴', layout="centered")
 
-    st.header("Youtube Blog Generator")
+    # Apply styles
+    with open('./styles/styles.css') as file:
+        style = file.read()
+        st.markdown(f"""<style>{style}</style>""", unsafe_allow_html=True)
+        st.markdown("<h3 style='text-align: center;'>Welcome to the AI-powered Youtube Blog Generator</h3>", unsafe_allow_html=True)
 
-    # with st.form("my_form"):
-    #     st.write("Inside the form")
-
+    with st.form("my_form"):
+        st.write("To generate your blog post, simply enter the YouTube link below. Our form will extract the necessary details from the video to create a draft for your blog post. ")
+        st.text_input("Enter YoutubeLink",
+        key="link")
+        st.form_submit_button('Generate', type='primary')
 
 
 if __name__ == '__main__':
