583ec50abe6c94f7f8551b2f6c98bab844e2674f
Mar-Issah <masy370@gmail.com>
2024-06-03 22:01:30 -0600

    remove frontend and server folders

--- server/app/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:siya@localhost/youtube_blog'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY =  os.getenv("SECRET_KEY")
-    JWT_SECRET_KEY =  os.getenv("SECRET_KEY")
