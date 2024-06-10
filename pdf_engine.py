e0c6737890ac4350958d2a75bb000ca0c6b24ad5
Mar-Issah <masy370@gmail.com>
2024-06-09 18:21:42 -0600

    Create pdf_engine file

--- pdf_engine.py
+++ pdf_engine.py
@@ -1,3 +1,21 @@
 import os
-from llama_index import StorageContext, VectorStoreIndex, load_index_from_storage
-from llama_index.readers import PDFReader
+from llama_index.core import StorageContext, VectorStoreIndex, load_index_from_storage
+# PDFReader to read unstructures data i.e pdf file
+from llama_index.readers.file import PDFReader
+import os
+
+# this fxn accepts the document to persist and the path
+def get_index(docs, index_path):
+	index = None
+	# if the path exist then docs has been persisted then we simply load from the staorage
+	if not os.path.exists(index_path):
+		print(index_path)
+		index = VectorStoreIndex.from_documents(docs, show_progress=True)
+		index.storage_context.persist(persist_dir=index_path)
+	else:
+		index = load_index_from_storage(StorageContext.from_defaults(persist_dir	=index_path))
+	return index
+
+loader = PDFReader()
+docs = loader.load_data(file=("./data/Africa.pdf"))
+print(docs)
